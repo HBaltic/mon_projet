1550b0c170bf48920586c2d89b724d90ebfcb62d
HBaltic <husref.baltic@gmail.com>
2017-06-13 15:11:38 +0200

    Ajout de la méthode Run2

--- src/main/java/run/Run.java
+++ src/main/java/run/Run.java
@@ -6,4 +6,9 @@ public class Run {
 	public static void main(String[] args) {
 		MyStringUtils.isNullOrEmpty("");
 	}
+	
+	public class Run2 {
+	public static void main(String[] args) {
+		MyStringUtils.isNullOrEmpty("run");
+}
 }
